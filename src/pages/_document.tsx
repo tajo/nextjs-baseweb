212147560f243b653f9ee755c35fdb7db7e872bd
Choongkyu Kim <choongkyu.kim+gh@gmail.com>
2022-11-11 20:47:55 -0800

    dep bump. update custom _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,39 @@
 import * as React from 'react';
-import Document, {Head, Html, Main, NextScript} from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import {Provider as StyletronProvider} from 'styletron-react';
-import {Server, Sheet} from 'styletron-engine-atomic';
-import {styletron} from '../styletron';
 
-class MyDocument extends Document<{stylesheets: Sheet[]}> {
-  static getInitialProps(props: any) {
-    // eslint-disable-next-line react/display-name
-    const page = props.renderPage((App: any) => (props: any) => (
-      <StyletronProvider value={styletron}>
-        <App {...props} />
-      </StyletronProvider>
-    ));
-    const stylesheets = (styletron as Server).getStylesheets() || [];
-    return {...page, stylesheets};
+import {styletron} from '../styletron';
+import {sheetT} from 'styletron-engine-atomic/lib/server/server';
+
+class MyDocument extends Document<{stylesheets: sheetT[]}> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyletronProvider value={styletron}>
+              <App {...props} />
+            </StyletronProvider>
+          ),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [<>{initialProps.styles}</>],
+    };
   }
 
   render() {
     return (
       <Html>
-        <Head>
-          {this.props.stylesheets.map((sheet, i) => (
-            <style
-              className="_styletron_hydrate_"
-              dangerouslySetInnerHTML={{__html: sheet.css}}
-              media={sheet.attrs.media}
-              data-hydrate={sheet.attrs['data-hydrate']}
-              key={i}
-            />
-          ))}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
